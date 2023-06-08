4e4b03820661261730f9561a62bd46e12e40038b
Pedro-HFelix <pf711363@gmail.com>
2023-06-08 09:12:49 -0300

    third commit

--- src/Main.java
+++ src/Main.java
@@ -11,17 +11,20 @@ public class Main {
         curso1.setTitulo("curso Java");
         curso1.setDescricao("descrição curso Java");
         curso1.setCargaHoraria(8);
-        System.out.println(curso1);
 
         Curso curso2 = new Curso();
         curso2.setTitulo("curso JavaScript");
         curso2.setDescricao("descrição curso JavaScript");
         curso2.setCargaHoraria(4);
+
+        System.out.println(curso1);
         System.out.println(curso2);
+
         Mentoria mentoria1 = new Mentoria();
         mentoria1.setTitulo("mentoria de Java");
         mentoria1.setDescricao("descrição mentoria de Java");
         mentoria1.setData(LocalDate.now());
+
         System.out.println(mentoria1);
     }
 }
